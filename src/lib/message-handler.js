3aed77ed3cb14553c2c77ba35e1fe1a40b39a59f
robsonrosa <robsonrosa@outlook.com>
2016-07-30 02:24:34 -0300

    Adding support for params within triggers

--- src/lib/message-handler.js
+++ src/lib/message-handler.js
@@ -2,13 +2,14 @@
 
 exports.callTasks = callTasks;
 
-var Stringfy = require('string');
+var trigger = require(__base + 'src/lib/trigger');
 
 function callTasks(message, hubot) {
    hubot.core.tasks.forEach(function(task) {
-      if (accept(message, task.trigger)) {
+      var acceptance = trigger.check(message.text, task.trigger);
+      if (acceptance.ok) {
          var handler = getHandler(hubot, task);
-         handler.process(message, hubot, task);
+         handler.process(message, hubot, task, acceptance.params);
       }
    });
 }
@@ -16,13 +17,3 @@ function callTasks(message, hubot) {
 function getHandler(hubot, task) {
    return hubot.core.handlers.find(h => h.key === task.handler);
 }
-
-function accept(message, phrase) { 
-   return normalize(message.text) == normalize(phrase);
-}
-
-function normalize(text) {
-   return Stringfy(text).trim().latinise().s.toLowerCase();
-}
-
-
---
+++ src/lib/trigger.js
@@ -0,0 +1,54 @@
+'use strict';
+
+exports.check = check;
+
+var stringfy = require('string');
+
+function check(message, trigger) {
+   return new Trigger('$').accept(message, trigger);
+}
+
+class Trigger {
+   constructor(placeholder) {
+      this.placeholder = placeholder;
+   }
+
+   accept(message, phrase) {
+      message = this.normalize(message);
+      phrase = this.normalize(phrase);
+
+      if (!phrase.startsWith(this.placeholder) && phrase.indexOf(this.placeholder) > 0) {
+         return this.acceptWithParams(message, phrase);
+      }
+
+      return this.acceptWithoutParams(message, phrase);
+   }
+
+   acceptWithParams(message, phrase) {
+      var params = this.getParams(message, phrase);
+      var count = (phrase.match(new RegExp('\\' + this.placeholder,'g')) || []).length;
+      return { ok: params.length === count, params: params }
+   }
+
+   acceptWithoutParams(message, phrase) {
+      return { ok: message == phrase };
+   }
+
+   normalize(text) {
+      return stringfy(text).trim().latinise().s.toLowerCase();
+   }
+
+   getParams(message, phrase) {
+      // get $ to $  >  ['get ', ' to ']
+      var triggerParts = phrase.split(this.placeholder).filter(s => s);
+      
+      // get alpha to beta  >  'alpha$beta'
+      triggerParts.reduce((previous, next) => { 
+         message = message.replace(previous, this.placeholder);
+         message = message.replace(next, this.placeholder);
+      });
+
+      // 'alpha$beta'  >  ['alpha', 'beta']
+      return message.split(this.placeholder).filter(s => s);
+   }
+}
